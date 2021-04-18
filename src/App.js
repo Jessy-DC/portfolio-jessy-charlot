6640241c195f25dff8a93c7f52483cdd17b8ad54
DC <jessy.charlot@outlook.fr>
2021-04-19 00:32:07 +0200

    Add about and update styles

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './index.scss'
 import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/home";
 import Footer from "./components/footer/footer";
+import About from "./components/about/about";
 
 function App() {
   return (
     <div className="App">
         <Navbar />
         <Home />
+        <About />
         <Footer />
     </div>
   );
---
+++ src/components/about/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div id="about" className="about">
+            <h2>Qui suis-je ?</h2>
+            <div className="summary">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad adipisci alias atque, beatae corporis,
+                    enim omnis quae reiciendis rem repudiandae sapiente suscipit, velit. Aliquam beatae, consequatur, delectus deleniti
+                    earum eligendi enim impedit iusto necessitatibus obcaecati perferendis quis, vitae voluptatibus? Aliquid dolore
+                    impedit ipsum quae repellendus sed, voluptates! Dicta, ipsum!</p>
+            </div>
+        </div>
+    )
+}
